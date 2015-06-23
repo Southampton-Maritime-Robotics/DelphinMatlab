59cb862f117cb71dd73a8e60f9ca07621076edf5
TanakitkornK <kt1e11@soton.ac.uk>
2015-06-23 15:19:36 +0100

    put in the password automatically when sending the files to delphin2

--- sendCode2.m
+++ sendCode2.m
@@ -4,7 +4,7 @@
 % may need to add a verification step to properly sync two folders.
 
 % send entire pkgs to delphin2 at once {navigation,delphin2_misison,hardware_interfaces,lowlevel_controllers,xsens_driver}
-system('scp -r ~/DelphinROSv3/src/{delphin2_mission,hardware_interfaces,lowlevel_controllers,xsens_driver} delphin2@delphin2:~/DelphinROSv3/src/'); %>temp.txt
+system('sshpass -p 012345 scp -r ~/DelphinROSv3/src/{delphin2_mission,hardware_interfaces,lowlevel_controllers,xsens_driver} delphin2@delphin2:~/DelphinROSv3/src/'); %>temp.txt
 
 x = fix(clock);
 fprintf('Code sent at time: %d:%d:%d \n\r',x(4),x(5),x(6))
