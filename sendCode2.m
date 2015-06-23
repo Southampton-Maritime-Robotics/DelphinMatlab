9809a2262eab333958d8bd5aa05100016f5e57ea
kantapon <kt1e11@soton.ac.uk>
2015-06-23 13:00:12 +0100

    removed unneccessary lines and use a relative path when refer to the folder

--- sendCode2.m
+++ sendCode2.m
@@ -1,23 +1,10 @@
-% this code help to transfer file from delphin2laptop to delphin2
+% main function to send the code to delphin2
 % note1: files with same name will be replaced
-% note2: files that has been delected from delphin2laptop will still
-% available on the delhpin2 HD.
+% note2: files that has been delected from delphin2laptop will still available on the delhpin2 HD.
+% may need to add a verification step to properly sync two folders.
 
 % send entire pkgs to delphin2 at once {navigation,delphin2_misison,hardware_interfaces,lowlevel_controllers,xsens_driver}
-system('scp -r /home/delphin2laptop/DelphinROSv3_mock_up/src/{delphin2_mission,hardware_interfaces,lowlevel_controllers,xsens_driver} delphin2@delphin2:~/DelphinROSv3/src/'); %>temp.txt
-
-% % % send navigation pkg to delphin2 {launch,scripts,msg,maps}
-% % system('scp -r /home/delphin2laptop/DelphinROSv3_mock_up/src/navigation/{launch,scripts,msg} delphin2@delphin2:~/DelphinROSv3/src/navigation/'); %>temp.txt
-% % 
-% % % send de0123lphin2_mission pkg to delphin2 {launch,scripts,msg}
-% % system('scp -r /home/delphin2laptop/DelphinROSv3_mock_up/src/delphin2_mission/{launch,scripts,msg} delphin2@delphin2:~/DelphinROSv3/src/delphin2_mission/'); %>temp.txt
-% % 
-% % % send hardware_interfaces pkg to delphin2 {launch,scripts,msg}
-% % system('scp -r /home/delphin2laptop/DelphinROSv3_mock_up/src/hardware_interfaces/{launch,scripts,msg} delphin2@delphin2:~/DelphinROSv3/src/hardware_interfaces/'); %>temp.txt
-% % 012345
-
-% % % send lowlevel_controllers pkg to delphin2 {launch,scripts,msg}
-% % system('scp -r /home/delphin2laptop/DelphinROSv3_mock_up/src/lowlevel_controllers/{launch,scripts,msg} delphin2@delphin2:~/DelphinROSv3/src/lowlevel_controllers/'); %>temp.txt
+system('scp -r ~/DelphinROSv3/src/{delphin2_mission,hardware_interfaces,lowlevel_controllers,xsens_driver} delphin2@delphin2:~/DelphinROSv3/src/'); %>temp.txt
 
 x = fix(clock);
 fprintf('Code sent at time: %d:%d:%d \n\r',x(4),x(5),x(6))
