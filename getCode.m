a03b4881b007236a590e295b63304e1879e9505c
TanakitkornK <kt1e11@soton.ac.uk>
2015-06-23 13:22:14 +0100

    correct the sequence of the folder name: year-month-day_hour-min_sec

--- getCode.m
+++ getCode.m
@@ -1,5 +1,5 @@
 % get code from delphin2 and keep locally
-dirName = datestr(now,'yyyy-MM-dd_HH-mm-ss');
+dirName = datestr(now,'yyyy-mm-dd_HH-MM-ss');
 mkdir('~/Delphin_LoadedCode',dirName)
 cmd = ['scp -r delphin2@delphin2:~/DelphinROSv3/src/{delphin2_mission,lowlevel_controllers,hardware_interfaces} ~/Delphin_LoadedCode/',dirName];
 system(cmd);
