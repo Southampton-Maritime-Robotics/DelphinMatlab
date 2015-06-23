1faf7f6bf0af40ba4e2c523a7a76a93329e189a7
kantapon <kt1e11@soton.ac.uk>
2015-06-23 14:59:18 +0100

    auto input the password when getting the code

--- getCode.m
+++ getCode.m
@@ -1,5 +1,7 @@
 % get code from delphin2 and keep locally
+password = '012345';
 dirName = datestr(now,'yyyy-mm-dd_HH-MM-ss');
 mkdir('~/Delphin_LoadedCode',dirName)
-cmd = ['scp -r delphin2@delphin2:~/DelphinROSv3/src/{delphin2_mission,lowlevel_controllers,hardware_interfaces} ~/Delphin_LoadedCode/',dirName];
+cmd = ['sshpass -p ', password, ' scp -r delphin2@delphin2:~/DelphinROSv3/src/{delphin2_mission,lowlevel_controllers,hardware_interfaces} ~/Delphin_LoadedCode/',dirName];
 system(cmd);
+cmd = ['sshpass -p ', password, ' scp -r delphin2@delphin2:',file,' ',logFolder];
