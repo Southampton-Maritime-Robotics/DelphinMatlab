022c0921503d58e05830e6940782149ac733a95d
TanakitkornK <kt1e11@soton.ac.uk>
2015-06-23 15:18:15 +0100

    removed the duplicated line that causes the error

--- getCode.m
+++ getCode.m
@@ -4,4 +4,3 @@ dirName = datestr(now,'yyyy-mm-dd_HH-MM-ss');
 mkdir('~/Delphin_LoadedCode',dirName)
 cmd = ['sshpass -p ', password, ' scp -r delphin2@delphin2:~/DelphinROSv3/src/{delphin2_mission,lowlevel_controllers,hardware_interfaces} ~/Delphin_LoadedCode/',dirName];
 system(cmd);
-cmd = ['sshpass -p ', password, ' scp -r delphin2@delphin2:',file,' ',logFolder];
