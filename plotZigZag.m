fdf79f6c46ef35fc193644657cdce3506c7e345f
SENG <kt1e11@soton.ac.uk>
2016-06-12 01:11:37 +0100

    remove limit of x axis

--- plotZigZag.m
+++ plotZigZag.m
@@ -7,7 +7,6 @@ heading = compassLog(:,2)-headingInit;
 timeTail = tailLog(:,1)-timeStart;
 demand_cs = tailLog(:,6);
 
-
 timeThruster = thrusterLog(:,1)-timeStart;
 demand_th = thrusterLog(:,4);
 
@@ -15,5 +14,5 @@ figure(1); clf; hold on;
 plot(timeCompass,heading,'k--')
 plot(timeTail,demand_cs,'r')
 plot(timeThruster,demand_th/1800*30,'g')
-xlim([-20,100])
+% xlim([-20,100])
 legend('heading','u_{cs}','u_{th}')
